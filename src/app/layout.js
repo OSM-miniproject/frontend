44293a6cb730ba94676a3a119a61b6a1d43c2493
thundersp <thundersp20504@gmail.com>
2024-09-23 11:50:29 +0530

    More about layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        header1
+        {children}
+        footer1
+      </body>
     </html>
   )
 }
